9585cacae6f7b3f301d9132845494a4827813933
Matteo Santoro <m.santoro@cosmobile.com>
2023-07-24 12:03:41 +0200

    * something from pc

--- ActorProgramming/src/main/java/it/unibo/ppc/akka/Employee.java
+++ ActorProgramming/src/main/java/it/unibo/ppc/akka/Employee.java
@@ -11,7 +11,14 @@ import akka.actor.typed.javadsl.Behaviors;
 
 public class Employee extends AbstractBehavior<Pm.Ordered>{
 
-    public static class Report {} 
+    public static class Report {
+        public Map<String, Integer> response; 
+        
+        public Report(String filename, int numberOfLines){
+            this.response = new HashMap<>();
+        }
+
+    } 
     private Employee(ActorContext<Ordered> context) {
         super(context);
     }
@@ -27,10 +34,12 @@ public class Employee extends AbstractBehavior<Pm.Ordered>{
         return newReceiveBuilder().onMessage(Pm.Ordered.class, this::onMsgReceived).build();
     }
     
+
     private Behavior<Pm.Ordered> onMsgReceived(Pm.Ordered message){
         getContext().getLog().info("Gonna mess around wtih: " + message.task);
         // message.from.tell(new Ordered(name, getContext(), new ArrayList<String>()));
         // message.from.tell(null);
+
         // getContext().getLog().info("Got it Pm -{}", this.name, getContext(), getClass());
         return this;
     }
